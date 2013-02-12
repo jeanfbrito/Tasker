69695dd041bf4ffcc691bfafd96b4d57a96328dc
vagrant <jeanfbrito@gmail.com>
2013-02-12 13:24:00 +0100

    installed Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasker::Application.config.secret_token = 'bcc7c02d5ea7e0e214f5b71a14f8783c359a93f264fd2c18a6ddd6a39b3964e416d584ac5611f305e740a82541e10af608e43d4d1daf794ee37aacfbd443e65d'
