5c7231524fbfb83e6a8db7fd29bc21347db88eeb
vagrant <jeanfbrito@gmail.com>
2013-02-12 16:27:15 +0100

    created notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130212122453) do
+ActiveRecord::Schema.define(:version => 20130212123612) do
+
+  create_table "notes", :force => true do |t|
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "priorities", :force => true do |t|
     t.integer  "level"
