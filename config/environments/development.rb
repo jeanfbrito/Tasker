8a1d2c9153162310f08e11c022a6384f846b43d7
vagrant <jeanfbrito@gmail.com>
2013-03-12 19:23:31 +0100

    installed the layout

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Tasker::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
