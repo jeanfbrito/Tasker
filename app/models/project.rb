e60e2bb0d36ba00a185e693e4d477abd35958768
vagrant <jeanfbrito@gmail.com>
2013-03-13 15:34:43 +0100

    Now breadcrumb shows the project name and project index only show root tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,18 +1,18 @@
 class Project < ActiveRecord::Base
 
-	attr_accessible :title, :description
+  attr_accessible :title, :description
 
-	belongs_to	:state
-	belongs_to	:priority
+  belongs_to  :state
+  belongs_to  :priority
 
-	has_many	:project_notes
-	has_many 	:notes, :through => :project_notes
-	has_many 	:project_tasks
-	has_many	:tasks, :through => :project_tasks
+  has_many  :project_notes
+  has_many  :notes, :through => :project_notes
+  
+  has_many :tasks
 
   def have_tasks?
-  	if tasks.count > 0
-    	return true
+    if tasks.count > 0
+      return true
     end
     false
   end
