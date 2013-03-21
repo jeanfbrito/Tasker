663fdc0e6970815f7cb06c80f29afa1c013d6798
vagrant <jeanfbrito@gmail.com>
2013-03-21 05:23:57 +0100

    try to resolve the better_errors problem

--- config/initializers/better_errors.rb
+++ config/initializers/better_errors.rb
@@ -1,2 +1,4 @@
 # Allow usage on Vagrant
-BetterErrors::Middleware.allow_ip! "10.0.2.2" if defined? BetterErrors && Rails.env == :development
+if Rails.env == :development
+	BetterErrors::Middleware.allow_ip! "10.0.2.2" if defined? BetterErrors && Rails.env == :development
+end
