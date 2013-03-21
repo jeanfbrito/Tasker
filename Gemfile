458d6f78c13a2165000806784553016a52554952
vagrant <jeanfbrito@gmail.com>
2013-03-21 04:58:41 +0100

    updates the user informations on every new login

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :production do
 end
 
 group :development do
+  gem 'meta_request'
   gem 'sqlite3'
   gem "better_errors"
   gem "binding_of_caller"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320172203) do
+ActiveRecord::Schema.define(:version => 20130321033829) do
 
   create_table "notes", :force => true do |t|
     t.string   "title"
@@ -30,13 +30,6 @@ ActiveRecord::Schema.define(:version => 20130320172203) do
   create_table "project_notes", :force => true do |t|
     t.integer  "project_id"
     t.integer  "note_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "project_tasks", :force => true do |t|
-    t.integer  "project_id"
-    t.integer  "task_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -61,13 +54,6 @@ ActiveRecord::Schema.define(:version => 20130320172203) do
   create_table "task_notes", :force => true do |t|
     t.integer  "task_id"
     t.integer  "note_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "task_subtasks", :force => true do |t|
-    t.integer  "task_id"
-    t.integer  "subtask_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -110,7 +96,10 @@ ActiveRecord::Schema.define(:version => 20130320172203) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
-    t.string   "surname"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "picture"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
