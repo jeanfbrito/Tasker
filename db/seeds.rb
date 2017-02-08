eb43d3a31f3bd1645cfec69e6e694785cf7c070c
Patrick Müller <patrick.e.muller@gmail.com>
2017-02-08 18:48:34 -0200

    Add basic seed for Users and Projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "========= >>>> Creating basic Users\n"
+10.times do |i|
+  User.create!(
+    email: "user_example_#{i}@test.com",
+    first_name: 'User',
+    last_name: "Example #{i}",
+    password: '123123123'
+  )
+end
+
+puts "========= >>>> Creating basic Projects\n"
+10.times do |i|
+  project = Project.create!(
+    title: "Project #{i}"
+  )
+
+  # Add people to projects
+  UserProject.create!(project: project, user: User.all.sample)
+end
